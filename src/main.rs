50ed86f19b9b85f373005b0b836505a4517e51e2
Arnaud Bergeron <abergeron@gmail.com>
2025-10-07 15:30:52 -0400

    Initial code

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+use anyhow::Result;
+use exec::{Command, Error};
+use log;
+use regex::Regex;
+use std::env;
+use std::ffi::OsString;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::process::exit;
+
+fn lines(path: OsString) -> Result<Vec<String>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    let lines: std::result::Result<Vec<String>, std::io::Error> = reader.lines().collect();
+    Ok(lines?)
+}
+
+fn run_command(command: &str) -> Error {
+    Command::new("sh").arg("-c").arg(command).exec()
+}
+
+fn fail() -> ! {
+    println!("denied");
+    exit(1)
+}
+
+fn main() {
+    let rules_file = match env::args_os().nth(1) {
+        Some(path) => path,
+        None => {
+            log::error!("No rules file argument recieved");
+            fail()
+        }
+    };
+    let original_command = match env::var("SSH_ORIGINAL_COMMAND") {
+        Ok(value) => value,
+        Err(e) => {
+            log::error!("Cannot retrive original command: {e}");
+            fail()
+        }
+    };
+    let rules = match lines(rules_file) {
+        Ok(v) => v,
+        Err(e) => {
+            log::error!("Error reading rules file: {e}");
+            fail()
+        }
+    };
+    log::info!("Orignal command: {original_command}");
+    for rule in rules {
+        // Skip comments
+        if rule.trim_start().starts_with("#") {
+            continue;
+        }
+        match Regex::new(&rule) {
+            Ok(r) => {
+                if r.is_match(&original_command) {
+                    log::info!("Matched rule {rule}, running command");
+                    let err = run_command(&original_command);
+                    log::error!("Error running command: {err}");
+                    println!("error");
+                    exit(1);
+                }
+            }
+            Err(e) => {
+                log::info!("Invalid regex in rules file: {rule}: {e}");
+                fail();
+            }
+        }
+    }
+    log::info!("Command did not match any rule");
+    fail()
+}
